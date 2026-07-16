66f676dfa39897cf63be1515b2eba44de116e9fb
Şeyma İbiş <ibisseyma38@gmail.com>
2026-07-16 09:34:09 +0300

    ders5 bitti

--- index.php
+++ index.php
@@ -1,6 +1,53 @@
 <?php
 
-echo "merhaba dünya";
-echo "<br>";
-echo "merhaba php ile web programlama"
+$num1 = 34;
+$num2 = 45;
+
+if($num1>$num2){
+    echo "büyük sayımız $num1 'dir";
+} else {
+    echo "büyük sayımız $num2 'dir";
+}
+$vize = 45;
+$final = 55;
+$ortalama = ($vize * 0.4)+($final * 0.6);
+if($ortalama>50){
+    echo "Dersi geçtiniz. Ortalamanız = $ortalama";
+}else{
+    echo "Dersten kaldınız. Ortalamanız = $ortalama";
+}
+
+$vize = 25;
+$final = 60;
+$ortalama = ($vize * 0.4)+($final * 0.6);
+if($ortalama>50){
+    echo "Dersi geçtiniz. Ortalamanız = $ortalama";
+}else{
+    echo "Dersten kaldınız. Ortalamanız = $ortalama";
+}
+
+$number = 65;
+
+if($number % 2 == 0) {
+    echo "Sayımız çift sayıdır" ;
+}else{
+    echo "Sayımız tek sayıdır" ;
+}
+
+$username = "Şeyma" ;
+$password = "12345" ;
+
+$girilen_user = "Sıla" ;
+$girilen_parola = "12345" ;
+ 
+if (($username==$girilen_user)&&($password==$girilen_parola)){
+    echo "Giriş Başarılı";
+}else{
+    echo "Giriş Başarısız";
+}
+
+
+
+
+
 ?>
