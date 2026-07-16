e420c40f1e912c579d52099c6f154feda1ac3d6e
Şeyma İbiş <ibisseyma38@gmail.com>
2026-07-16 11:40:45 +0300

    ders6 bitti

--- index.php
+++ index.php
@@ -1,6 +1,40 @@
 <?php
+$numbers = array (2, 3, 4, 5, 6, 23, 43, 12, 76, 88, 75);
 
-echo "merhaba dünya";
-echo "<br>";
-echo "merhaba php ile web programlama"
+for ($i=0; $i <count($numbers); $i++) {
+echo $numbers[$i]."<br>";
+}
+echo "**********" . "<br>";
+foreach($numbers as $number) { echo $number. "<br>";
+}
+
+echo "**********" . "<br>";
+for ($i=0; $i <count($numbers); $i++) {
+if ($numbers[$i]%2==0) {
+    echo "çift sayıdır ". $numbers[$i]."<br>";
+    }
+}
+
+echo "**********" . "<br>";
+for ($i=0; $i <count($numbers); $i++) {
+if ($numbers[$i]%3==0) {
+    continue;
+}else{
+    echo "3'e bölünmeyen sayılar " . $numbers[$i] . "<br>";
+    }
+}
+
+$toplam = 0;
+
+echo "************" . "<br>";
+for ($i=0; $i <count($numbers);
+$i++) {
+if ($numbers[$i]%5==0){
+$toplam += $numbers[$i];
+}
+}
+  echo "5'e tam bölüneilen sayıların toplamı " . $toplam . "<br>";
+
+
+
 ?>
