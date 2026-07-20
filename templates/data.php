40dd651d27d1e8f75bee66ce5e9f363df08e3fc9
Şeyma İbiş <ibisseyma38@gmail.com>
2026-07-20 16:57:37 +0300

    ders9 bitti

---
+++ templates/data.php
@@ -0,0 +1,62 @@
+<?php
+
+$product = array(
+    "1" => array(
+        "productName" => "Iphone14",
+        "productPrice" => 55000,
+        "productImg" => "../images/iphone14.jpg",
+        "productStok" => 25
+    ),
+    "2" => array(
+        "productName" => "Iphone13",
+        "productPrice" => 45000,
+        "productImg" => "../images/iphone13.jpg",
+        "productStok" => 30
+    ),
+    "3" => array(
+        "productName" => "Iphone12",
+        "productPrice" => 35000,
+        "productImg" => "../images/iphone12.jpg",
+        "productStok" => 0
+    ),
+    "4" => array(
+        "productName" => "Samsung S21",
+        "productPrice" => 50000,
+        "productImg" => "../images/samsungs21.jpg",
+        "productStok" => 15
+    ),
+    "5" => array(
+        "productName" => "Samsung S20",
+        "productPrice" => 40000,
+        "productImg" => "../images/samsungs20.jpg",
+        "productStok" => 10
+    ),
+    "6" => array(
+        "productName" => "Samsung S18",
+        "productPrice" => 35000,
+        "productImg" => "../images/samsungs18.jpg",
+        "productStok" => 2
+    ),
+    "7" => array(
+        "productName" => "Lenovo",
+        "productPrice" => 19000,
+        "productImg" => "../images/lenovo.jpg",
+        "productStok" => 0
+    ),
+    "8" => array(
+        "productName" => "xiaomi",
+        "productPrice" => 28000,
+        "productImg" => "../images/xiaomi.jpg",
+        "productStok" => 12
+    ),
+    "9" => array(
+        "productName" => "Nokia",
+        "productPrice" => 5000,
+        "productImg" => "../images/nokia.jpeg",
+        "productStok" => 1
+    ),
+);
+$categories = array("Cep Telefonu", "Notebook", "Beyaz Eşya", "Küçük Ev Aletleri", "Kişisel Bakım Ürünleri");
+$title = "E-Ticaret";
+
+?>
---
+++ templates/meta.php
@@ -0,0 +1,20 @@
+<?php include "data.php"?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name ="viewport" content = "width=device-width, initial-scale=1">
+<title><?php echo $title ?></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+<style>
+.card-img-top{
+    width:100%;
+    height:250px;
+    object-fit:contain;
+    background:#fff;
+    padding:10px;
+}
+</style>
+</head>
+
+<body>
